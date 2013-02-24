37fac2814b947a5a108f69b2440664fb601fa0c3
Joel Vieira <vieirajoel@gmail.com>
2013-02-24 09:47:40 -0300

    Teste

--- lib/brcobranca/boleto/base.rb
+++ lib/brcobranca/boleto/base.rb
@@ -12,7 +12,7 @@ module Brcobranca
 
       # Validações do Rails 3
       include ActiveModel::Validations
-
+      # Teste
       # <b>REQUERIDO</b>: Número do convênio/contrato do cliente junto ao banco emissor
       attr_accessor :convenio
       # <b>REQUERIDO</b>: Tipo de moeda utilizada (Real(R$) e igual a 9)

dc6f7655b5a7e28596ae0c4efa77849b2cfa3efc
Joel Vieira <vieirajoel@gmail.com>
2013-04-05 23:43:40 -0300

    Cllk

--- lib/brcobranca/boleto/template/rghost.rb
+++ lib/brcobranca/boleto/template/rghost.rb
@@ -136,7 +136,7 @@ module Brcobranca
           doc.image(boleto.logoempresa, :x => '0.5 cm', :y => '27 cm', :zoom => 80)
           #Composição da arrecadação
           doc.moveto :x => '10 cm' , :y => '27 cm'
-          doc.show "#{boleto.composicao.print}"
+          doc.show "#{boleto.composicao_print}"
           #doc.moveto :x => '20 cm' , :y => '27 cm'
           #doc.show boleto.jj
           #boleto.composicao.each { |a| doc.show a, " " }
